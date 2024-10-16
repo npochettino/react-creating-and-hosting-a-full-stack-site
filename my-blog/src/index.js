544b9af7cd033b65cf866f264aba009c21206c50
Nicolas Pochettino <nicolas.pochettino@santexgroup.com>
2024-10-16 10:21:33 -0300

    adding user authetication with Firebase Auth

--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7zZovaVBon8hKkuRWAyzz_L6MwXauQrs",
+  authDomain: "my-react-blog-9a1c5.firebaseapp.com",
+  projectId: "my-react-blog-9a1c5",
+  storageBucket: "my-react-blog-9a1c5.appspot.com",
+  messagingSenderId: "125512458100",
+  appId: "1:125512458100:web:d715a5e702659c9c3b5621"
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,29 +1,39 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from 'axios';
+import useUser from "../hooks/useUser.js";
 import articles from './article-content.js';
 import CommentsList from "../components/CommentsList.js";
 import AddCommentForm from "../components/AddCommentForm.js";
 import NotFoundPage from "./NotFoundPage.js";
 
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [], canUpvote: false });
+    const { canUpvote } = articleInfo;
     const { articleId } = useParams();
+
+    const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
+            const token = user && await user.getIdToken();
+            const headers = token ? { authtoken: token } : {}
+            const response = await axios.get(`/api/articles/${articleId}`, { headers });
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
 
-        loadArticleInfo();
-    }, []);
+        if (isLoading) {
+            loadArticleInfo();
+        }
+    }, [isLoading, user]);
 
     const article = articles.find(article => article.name === articleId);
 
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {}
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers });
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
@@ -36,18 +46,22 @@ const ArticlePage = () => {
         <>
         <h1>{article.title}</h1>
         <div className="upvotes-section">
-            <button onClick={addUpvote}>Upvote</button>
+            {user 
+                ? <button onClick={addUpvote}>{ canUpvote ? 'Upvote' : 'Already Upvoted' }</button>
+                : <button>Log in to upvote</button>}
             <p>This article has {articleInfo.upvotes} upvote(s)</p>
         </div>
         
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
-        <AddCommentForm
-            articleName={articleId}
-            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
-        <CommentsList comments={articleInfo.comments} />
-
+        {user
+            ?
+            <AddCommentForm
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+            : <button>Log in to add a comment</button>}
+            <CommentsList comments={articleInfo.comments} />
         </>
     );
 }
